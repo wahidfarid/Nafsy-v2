e87226839c79183b1c2facdd3ad87b5fbdc0f6e8
Wahid Magdy <wahidmagdy@gmail.com>
2021-02-19 10:58:46 +0200

    Simple creation of new points

--- app/controllers/knowledgebase_controller.rb
+++ app/controllers/knowledgebase_controller.rb
@@ -17,11 +17,22 @@ class KnowledgebaseController < ApplicationController
     @p = Point.find(request.params['id'])
   end
 
+  def new
+  end
+
   def update
     @p = Point.find(request.params['id'])
     @params = params.permit(:title, :content)
     @p.title = @params['title'] if !@params['title'].nil?
     @p.content = @params['content'] if !@params['content'].nil?
+    @p.save
+
+    render "show"
+  end
+
+  def create
+    point_params = params.permit(:title, :content)
+    @p = Point.new(title: point_params['title'], content: point_params['content'])
     @p.save
 
     render "show"
