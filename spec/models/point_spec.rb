1f296342572ede663d0fc954c90ffaea46603155
Wahid Magdy <wahidmagdy@gmail.com>
2021-02-26 23:15:01 +0200

    Validate presence of title in data point
    Added an rspec test to test validation of data point.
    this made me realize i never required the presence
    of the title attribute in the model, so i've added
    that requirement in the model

--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Point, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a title" do
+    point = Point.new(title: nil)
+    expect(point).to_not be_valid
+  end
 end
